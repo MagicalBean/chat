3b692cf6d25aa4360c5bd79fdd096d70c8a07dfb
Ben <ben.weisz13@gmail.com>
2020-09-12 22:03:49 -0700

    Sending messages!

--- client/src/Pages/home/Messages.js
+++ client/src/Pages/home/Messages.js
@@ -1,10 +1,22 @@
-import React, { useEffect, Fragment } from 'react';
-import { Col } from 'react-bootstrap';
-import { gql, useLazyQuery } from '@apollo/client';
+import React, { useEffect, Fragment, useState, useRef } from 'react';
+import { Col, Form } from 'react-bootstrap';
+import { gql, useLazyQuery, useMutation } from '@apollo/client';
 
 import { useMessageDispatch, useMessageState } from '../../context/message';
 
 import Message from './Message';
+
+const SEND_MESSAGE = gql`
+  mutation sendMessage($to: String!, $content: String!) {
+    sendMessage(to: $to, content: $content) {
+      uuid
+      from
+      to
+      content
+      createdAt
+    }
+  }
+`;
 
 const GET_MESSAGES = gql`
   query getMessages($from: String!) {
@@ -21,11 +33,18 @@ const GET_MESSAGES = gql`
 export default function Messages() {
   const { users } = useMessageState();
   const dispatch = useMessageDispatch();
+  const [content, setContent] = useState('');
 
   const selectedUser = users?.find((u) => u.selected === true);
   const messages = selectedUser?.messages;
 
   const [getMessages, { loading: messagesLoading, data: messagesData }] = useLazyQuery(GET_MESSAGES);
+
+  const [sendMessage] = useMutation(SEND_MESSAGE, {
+    onCompleted: (data) =>
+      dispatch({ type: 'ADD_MESSAGE', payload: { username: selectedUser.username, message: data.sendMessage } }),
+    onError: (err) => console.log(err),
+  });
 
   useEffect(() => {
     if (selectedUser && !selectedUser.messages) {
@@ -42,14 +61,45 @@ export default function Messages() {
     }
   }, [messagesData]);
 
+  const submitMessage = (e) => {
+    e.preventDefault();
+
+    if (content.trim() === '' || !selectedUser) return;
+
+    setContent('');
+
+    // mutation for sending the message
+    sendMessage({ variables: { to: selectedUser.username, content } });
+  };
+
+  //#region SCROLL TO BOTTOM
+  const scrollMarkRef = useRef(null);
+
+  const scrollToBottom = () => {
+    console.log(scrollMarkRef);
+    scrollMarkRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    if (scrollMarkRef.current) {
+      scrollToBottom();
+    }
+  }, [messages]);
+  //#endregion
+
   let selectedChatMarkup;
   if (!messages && !messagesLoading) {
-    selectedChatMarkup = <p>Select a friend</p>;
+    selectedChatMarkup = <p className="info-text">Select a friend</p>;
   } else if (messagesLoading) {
-    selectedChatMarkup = <p>Loading..</p>;
+    selectedChatMarkup = <p className="info-text">Loading..</p>;
   } else if (messages.length > 0) {
     selectedChatMarkup = messages.map((message, index) => (
       <Fragment key={message.uuid}>
+        {index === 0 && (
+          <div className="invisible">
+            <hr className="m-0" ref={scrollMarkRef} />
+          </div>
+        )}
         <Message message={message} />
         {index === messages.length - 1 && (
           <div className="invisible">
@@ -59,12 +109,28 @@ export default function Messages() {
       </Fragment>
     ));
   } else if (messages.length === 0) {
-    selectedChatMarkup = <p>You are now connected! Send your first message</p>;
+    selectedChatMarkup = <p className="info-text">You are now connected! Send your first message</p>;
   }
 
   return (
-    <Col xs={10} md={8} className="messages-box d-flex flex-column-reverse">
-      {selectedChatMarkup}
+    <Col xs={10} md={8}>
+      <div className="messages-box">
+        <div className="d-flex flex-column-reverse">{selectedChatMarkup}</div>
+      </div>
+      <div>
+        <Form onSubmit={submitMessage}>
+          <Form.Group className="d-flex align-items-center">
+            <Form.Control
+              type="text"
+              className="message-input rounded-pill p-4 bg-secondary border-0"
+              placeholder="Type a message.."
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+            <i role="button" className="fas fa-paper-plane fa-2x text-primary ml-2" onClick={submitMessage} />
+          </Form.Group>
+        </Form>
+      </div>
     </Col>
   );
 }
